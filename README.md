98b1a309abdd6e4ead818a00d696df1affd0bf82
Brad <bjcost@gmail.com>
2021-08-05 11:39:41 -0400

    Created radar app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# JSSiteTemplate
-Site Template for JS projects
+# Radar
+
+Radar is a palindrome. A Palindrome is a workd, phrase, or sequence that reads the same backward as forward. Write a program that checks if the phrase entered is a palindrome.
--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let inputString = document.getElementById("userString").value;
+
+    inputString = inputString.toLowerCase().replace(/[^A-Za-z]/g, '');
+
+    let palindrome = palindromeString(inputString);
+
+    displayString(palindrome, inputString);
+}
+
+function palindromeString(inputString){
+    let palinString = [];
+
+    for (let i = inputString.length-1; i >= 0; i--){
+        palinString += inputString[i];
+    }
+
+    return palinString;
+
+}
+
+function displayString(palinString, inputString){
+    let orgString = inputString;
+    let palindromeString = palinString;
+
+    if (palinString === orgString){
+        document.getElementById("message").innerHTML = `Success, <strong>${palinString}</strong> is a Palindrome!`;
+        document.getElementById("alert").classList.remove("invisible");
+    } else {
+        document.getElementById("message").innerHTML = `Sorry, <strong>${palinString}</strong> is not a Palindrome!`;
+        document.getElementById("alert").classList.remove("invisible");
+    }
+
 }
